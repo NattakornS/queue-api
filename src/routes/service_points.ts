6db475db09d8eabb5389505b348716cc008ca1d5
Nattakorn Sanpabopit <ntks@pop-os.localdomain>
2020-11-17 16:00:33 +0700

    add logic at kios list

--- src/routes/service_points.ts
+++ src/routes/service_points.ts
@@ -4,14 +4,58 @@ import * as Knex from 'knex';
 import * as fastify from 'fastify';
 import * as HttpStatus from 'http-status-codes';
 import { Random } from "random-js";
+import * as moment from 'moment';
 
 import { ServicePointModel } from '../models/service_point';
+import { EzhospModel } from '../models/his/ezhosp';
+import { DhosModel } from '../models/his/dhos';
+import { HosxpModel } from '../models/his/hosxp';
+import { HiModel } from '../models/his/hi';
+import { HomcModel } from '../models/his/homc';
+import { UniversalModel } from '../models/his/universal';
+import { WuHisModel } from '../models/his/wuhis';
+import { KioskModel } from '../models/kiosk';
 
 const servicePointModel = new ServicePointModel();
+
+// modify
+// author: ntks
+// desc : filter service point by WUHIS
+const hisType = process.env.HIS_TYPE || 'universal';
+
+const kioskModel = new KioskModel();
+
+let hisModel: any;
+switch (hisType) {
+  case 'ezhosp':
+    hisModel = new EzhospModel();
+    break;
+  case 'dhos':
+    hisModel = new DhosModel();
+    break;
+  case 'hosxp':
+    hisModel = new HosxpModel();
+    break;
+  case 'hi':
+    hisModel = new HiModel();
+    break;
+  case 'homc':
+    hisModel = new HomcModel();
+    break;
+  case 'universal':
+    hisModel = new UniversalModel();
+    break;
+  case 'wuhis':
+    hisModel = new WuHisModel();
+    break;
+  default:
+    hisModel = new HosxpModel();
+}
 
 const router = (fastify, { }, next) => {
 
   var db: Knex = fastify.db;
+  const dbHIS: Knex = fastify.dbHIS;
 
   // get service point lists
   fastify.get('/', { preHandler: [fastify.authenticate] }, async (req: fastify.Request, reply: fastify.Reply) => {
@@ -27,14 +71,44 @@ const router = (fastify, { }, next) => {
 
   fastify.get('/kios', { preHandler: [fastify.authenticate] }, async (req: fastify.Request, reply: fastify.Reply) => {
     try {
+      // modify
+      // author: ntks
+      // desc : filter service point by WUHIS
+      const dateServ: any = moment().format('YYYY-MM-DD');
+      const localCodes: any = [];
+      const vn = []
+      const limit = +req.query.limit;
+      const offset = +req.query.offset;
+      var servicePointCode = req.query.kioskId || ''
+      const query: any = req.query.query || '';
+      
       var rs: any = await servicePointModel.listKios(db);
-      if(req.params.mode === 'new') {
-        rs = rs.filters(x=>x.service_point_id === 1000)
+      const rsLocalCode: any = await servicePointModel.getLocalCode(db);
+
+      rsLocalCode.forEach(v => {
+        localCodes.push(v.local_code);
+      });
+
+      if(req.query.mode === 'new') {
+        rs = rs.filter(x=>x.service_point_id === 1000 || x.service_point_id === 1001)
       } else {
         // filter by vn from his
-        
+        // if (req.params.kioskId) {
+        //   localCodes.push(req.params.kioskId)
+        //   servicePointCode.push()
+        // }
+        var avaliableClinics = await hisModel.getVisitList(dbHIS, dateServ, localCodes, vn, '', query)
+        console.log(avaliableClinics);
+        var local_codes = []
+        if (avaliableClinics.length > 0) {
+          local_codes = avaliableClinics.map(x=>x.clinic_code+'')
+        } else {
+          local_codes.push('1000')
+        }
+        console.log(local_codes,rs);
+        rs = rs.filter(x=>local_codes.includes(x.local_code))
       }
-
+      
       reply.status(HttpStatus.OK).send({ statusCode: HttpStatus.OK, results: rs })
     } catch (error) {
       fastify.log.error(error);
